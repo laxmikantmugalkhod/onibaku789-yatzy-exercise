ab30c7bd5ad7e578863720e8c8a44b7783f8b78e
laxmikant_mugalkhod <laxmikant.mugalkhod@aventior.com>
2025-05-31 13:27:35 +0530

    Add yatzy method with tests coverage

--- src/main/java/YatzyJava.java
+++ src/main/java/YatzyJava.java
@@ -1,8 +1,17 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class YatzyJava {
 
     public static int chance(int... dice) {
         return Arrays.stream(dice).sum();
+    }
+
+    public static int yatzy(int... dice) {
+        Map<Integer, Long> counts = Arrays.stream(dice)
+                .boxed()
+                .collect(Collectors.groupingBy(d -> d, Collectors.counting()));
+        return counts.containsValue(5L) ? 50 : 0;
     }
 }
--- src/test/java/YatzyJavaTest.java
+++ src/test/java/YatzyJavaTest.java
@@ -9,4 +9,10 @@ public class YatzyJavaTest {
         assertEquals(15, YatzyJava.chance(2, 3, 4, 5, 1));
     }
 
+    @Test
+    public void testYatzy() {
+        assertEquals(50, YatzyJava.yatzy(4, 4, 4, 4, 4));
+        assertEquals(0, YatzyJava.yatzy(4, 4, 4, 4, 3));
+    }
+
 }
