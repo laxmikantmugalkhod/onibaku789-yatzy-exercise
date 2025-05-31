987d86046ff11868329329adf78ad4c98dd1c23b
laxmikant_mugalkhod <laxmikant.mugalkhod@aventior.com>
2025-05-31 13:37:19 +0530

    Added ones to sixes methods with tests coverage

--- src/main/java/YatzyJava.java
+++ src/main/java/YatzyJava.java
@@ -14,4 +14,34 @@ public class YatzyJava {
                 .collect(Collectors.groupingBy(d -> d, Collectors.counting()));
         return counts.containsValue(5L) ? 50 : 0;
     }
+
+    public static int ones(int... dice) {
+        return sumOfNumber(dice, 1);
+    }
+
+    public static int twos(int... dice) {
+        return sumOfNumber(dice, 2);
+    }
+
+    public static int threes(int... dice) {
+        return sumOfNumber(dice, 3);
+    }
+
+    public static int fours(int... dice) {
+        return sumOfNumber(dice, 4);
+    }
+
+    public static int fives(int... dice) {
+        return sumOfNumber(dice, 5);
+    }
+
+    public static int sixes(int... dice) {
+        return sumOfNumber(dice, 6);
+    }
+
+    private static int sumOfNumber(int[] dice, int number) {
+        return (int) Arrays.stream(dice)
+                .filter(d -> d == number)
+                .count() * number;
+    }
 }
--- src/test/java/YatzyJavaTest.java
+++ src/test/java/YatzyJavaTest.java
@@ -15,4 +15,19 @@ public class YatzyJavaTest {
         assertEquals(0, YatzyJava.yatzy(4, 4, 4, 4, 3));
     }
 
+    @Test
+    public void testOnes() {
+        assertEquals(1, YatzyJava.ones(1, 2, 3, 4, 5));
+        assertEquals(2, YatzyJava.ones(1, 2, 1, 4, 5));
+    }
+
+    @Test
+    public void testTwosToSixes() {
+        assertEquals(4, YatzyJava.twos(1, 2, 3, 2, 6));
+        assertEquals(6, YatzyJava.threes(1, 2, 3, 2, 3));
+        assertEquals(12, YatzyJava.fours(4, 4, 4, 5, 5));
+        assertEquals(15, YatzyJava.fives(4, 4, 5, 5, 5));
+        assertEquals(18, YatzyJava.sixes(6, 5, 6, 6, 5));
+    }
+
 }
