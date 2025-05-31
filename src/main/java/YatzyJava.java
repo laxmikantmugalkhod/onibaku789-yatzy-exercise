367d9a5ba61f78f5189b828e5a3533505339bf87
laxmikant_mugalkhod <laxmikant.mugalkhod@aventior.com>
2025-05-31 12:16:26 +0530

    Implement chance() to sum all dice.

--- src/main/java/YatzyJava.java
+++ src/main/java/YatzyJava.java
@@ -1,2 +1,8 @@
+import java.util.Arrays;
+
 public class YatzyJava {
+
+    public static int chance(int... dice) {
+        return Arrays.stream(dice).sum();
+    }
 }
--- src/test/java/YatzyJavaTest.java
+++ src/test/java/YatzyJavaTest.java
@@ -1,2 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class YatzyJavaTest {
+
+    @Test
+    public void testChance() {
+        assertEquals(15, YatzyJava.chance(2, 3, 4, 5, 1));
+    }
+
 }
